d71c3ee1b93e906a43bc8f0e968b0654222d00aa
Andrey <samu111ray@gmail.com>
2020-01-08 03:12:11 +0200

    removed coments

--- src/components/JustComponent.js
+++ src/components/JustComponent.js
@@ -1,51 +1,63 @@
-import React, { Component } from "react";
+import React from "react";
 
-class JustComponent extends Component {
-    state = {
+const JustComponent = ({ cleanAll, onPost }) => {
+    let state = {
         name: "",
         sureName: ""
     };
 
-    onHendleChange = e => this.setState({ name: e.target.value })
 
-    onHendleChangeSureName = e => this.setState({ sureName: e.target.value })
 
-    sendDataToMainState = () => {
-        const { name, sureName } = this.state;
-        this.props.onPost(name, sureName);
+  
+
+    const { name, sureName } = state;
+    const onHendleChange = e => this.setState({ name: e.target.value });
+
+    const onHendleChangeSureName = e =>{
+        console.log(state.sureName)
+        return {...state, sureName: e.target.value}
+        
+    }
+        
+
+    const sendDataToMainState = () => {
+        const { name, sureName } = state;
+        onPost(name, sureName);
         this.setState({
             name: "",
             sureName: ""
         });
     };
 
-    render() {
-        // const {onPost} = this.props.bind(this)
-        const { name, sureName } = this.state;
-        return (
+    return (
+        <div>
             <div>
-                <div>
-                    <input
-                        onChange={this.onHendleChange}
-                        type="text"
-                        placeholder="Name"
-                        value={name}
-                    />
-               
-                    <input
-                        onChange={this.onHendleChangeSureName}
-                        type="text"
-                        placeholder="Surename"
-                        value={sureName}
-                    />
-                    <input type="button" onClick={this.sendDataToMainState} value="Log In"/>
-                    <input type="button" onClick={()=>this.props.cleanAll()} value="Clean All"/>
-                </div>
-                {/* <button onClick={this.sendDataToMainState}>Log</button> */}
-                {/* <button onClick={()=>this.props.cleanAll()}>Clean All</button> */}
+                <input
+                    onChange={onHendleChange}
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                />
+
+                <input
+                    onChange={onHendleChangeSureName}
+                    type="text"
+                    placeholder="Surename"
+                    value={sureName}
+                />
+                <input
+                    type="button"
+                    onClick={sendDataToMainState}
+                    value="Log In"
+                />
+                <input
+                    type="button"
+                    onClick={() => cleanAll()}
+                    value="Clean All"
+                />
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export { JustComponent };
